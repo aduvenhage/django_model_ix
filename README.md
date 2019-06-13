c0091579569dcc6b5ae1e6e0de3499cef8a252a1
Arno Duvenhage <aduvenhage@csir.co.za>
2019-06-13 15:52:18 +0200

    first upload

---
+++ README.md
@@ -0,0 +1,27 @@
+# Docker Machine CLI API for Python
+As part of my efforts to learn python I decided to create a docker-machine CLI wrapper that can be used from within Python applications to create and manage remote/cloud VM services using docker-machine.  The wrapper also has support for upping and monitoring services, using docker-compose.
+
+Key features of Python I had to learn:
+- calling subprocesses using `Popen`
+- multi-threading and locks
+
+## Docker Machine
+Docker-machine works well to provision remote VMs since it hides much of the boilerplate effort required to create a remote machine, manage keys and create a basic image with Docker installed.  All docker-engine settings are managed via environment variables, and it even makes it easy to secure copy and ssh on the remote VM (with the keys stored in the machine environment).  Docker-machine has built-in drivers for a whole range of cloud providers, including Amazon Web Services, Digital Ocean and Google Compute Engine.
+
+
+### Docker Machine Commands
+- create VM (ubuntu 18.04 LTS -- Digital Ocean): 
+```
+docker-machine create --driver digitalocean --digitalocean-image ubuntu-18-04-x64 --digitalocean-access-token=... do01
+```
+- activate docker env: `eval $(docker-machine env do01) .`
+- deactivate docker env: `eval $(docker-machine env -u)`
+- ssh into remote machine: `docker-machine ssh do01`
+- list machines: `docker-machine ls`
+- remove machines: `docker-machine rm do01`
+- provision a system: docker-machine (create --> ssh --> docker-compose up)
+- NOTE: Docker containers may not use volumes/shares/mounts. All shared data must be copied from Dockerfiles
+
+
+## Python Usage Examples
+
---
+++ import_export/model_yaml.py
@@ -0,0 +1,117 @@
+
+
+from django.contrib.postgres.fields.jsonb import JSONField
+from django.db import models
+from django.db.models import fields
+
+import tablib
+import os
+
+
+
+
+# return DB style formatted field value
+def exportField(f, instance):
+    return f.value_to_string(instance)
+
+
+# copy model fields to dictionary
+def exportDict(instance):
+    data = {}
+
+    for f in instance._meta.get_fields():
+        # ignore all 'ForeignObjectRel' field types
+        if isinstance(f, fields.related.ForeignObjectRel):
+            pass
+
+        # export 'ForeignKey' field types
+        elif isinstance(f, fields.related.ForeignKey):
+            data[f.name] = exportField(f, instance)
+
+        # ignore all 'ForeignObjectRel' field types (besides ForeignKey)
+        elif isinstance(f, fields.related.RelatedField):
+            pass
+
+        else:
+            data[f.name] = exportField(f, instance)
+
+    return data
+
+
+# write model content to file in YAML format
+def writeYamlFile(modelType, path):
+    objects = list(modelType.objects.all())
+    if objects:
+        headers = exportDict(objects[0]).keys()
+        dataset = tablib.Dataset(headers=headers)
+
+        for obj in objects:
+            attr = exportDict(obj)
+            dataset.append(attr.values())
+
+        # create file and write dataset (text/yaml)
+        filePath = os.path.join(path, modelType.__name__ + ".yaml")
+        with open(filePath, "w+") as fout:
+            fout.write(dataset.yaml)
+
+
+# write resource to file in YAML format (with error handling)
+def exportYaml(modelType, path):
+    try:
+        writeYamlFile(modelType, path)
+        print("Exported '%s' to '%s'." % (modelType.__name__, path))
+
+    except Exception as e:
+        print("Failed to export '%s' to '%s'. %s" % (modelType.__name__, path, str(e)))
+
+
+
+# import formatted value
+def importField(instance, f, data):
+    try:
+        # NOTE: sets the model column attribute value for related fields
+        if isinstance(f, fields.related.RelatedField):
+            setattr(instance, f.column, data[f.name])
+        else:
+            setattr(instance, f.name, data[f.name])
+
+    except:
+        pass
+
+
+# update model fields with dictionary
+def importDict(instance, data):
+    for f in instance._meta.get_fields():
+        # ignore all 'ForeignObjectRel' field types
+        if not isinstance(f, fields.related.ForeignObjectRel):
+            importField(instance, f, data)
+
+
+# read model content from YAML file
+def readYamlFile(modelType, path):
+    # open data set file
+    filePath = os.path.join(path, modelType.__name__ + ".yaml")
+
+    with open(filePath, "r") as fin:
+        # read yaml/text
+        yaml = fin.read()
+
+        # import data set from yaml
+        dataset = tablib.Dataset().load(yaml)
+
+        for row in dataset:
+            obj = modelType()
+            importDict(obj, dict(zip(dataset.headers, row)))
+
+            obj.save()
+
+
+# read model content from YAML file (with error handling)
+def importYaml(modelType, path):
+    try:
+        readYamlFile(modelType, path)
+        print("Imported to '%s' from '%s'." % (modelType.__name__, path))
+
+    except Exception as e:
+        print("Failed to import to '%s' from '%s'. %s" % (modelType.__name__, path, str(e)))
+
